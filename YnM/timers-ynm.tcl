754097cd056a7c8aed4d3bc4a14b1ab5281e9b9f
Markus Lajos <csibeszml@gmail.com>
2025-01-20 18:25:42 +0200

    Update timers-ynm.tcl

--- YnM/timers-ynm.tcl
+++ YnM/timers-ynm.tcl
@@ -8,19 +8,27 @@
 #&##  or redistribution of this code is prohibited without prior written consent. 
 #&##"For any injury or environmental emergency, call your local emergency number."
 #&################################################################################ 
-
-
-
-proc YnMMysql_Update {} {
+proc YnMMysql_Update {args} {
     global YnMGlobal
-    # Globális
+
+    # Globális változók betöltése
     foreach var $YnMGlobal {
         global $var
     }
-  if {[strfind YnM_Upload [timers]]} {foreach timr [timers] {if {[strfind YnM_Upload $timr]} {killtimer [lindex $timr 2]}}}
-  timer ${YnMMysqlUpdate} YnM_Upload
+
+    # Ellenőrizzük, hogy van-e már időzítő az YnM_Upload számára
+    if {[lsearch -exact [timers] YnM_Upload] != -1} {
+        foreach timr [timers] {
+            # Töröljük a megfelelő időzítőt
+            if {[lsearch -exact $timr YnM_Upload] != -1} {
+                killtimer [lindex $timr 2]
+	  }
+        }
+    }
+
+    # Új időzítő létrehozása
+    timer $YnMMysqlUpdate YnM_Upload
 }
-timer 1 YnMMysql_Update
-     
-set YnMGlobal [concat $YnMGlobal {YnMMysql_Update}]
+
+timer 0 YnMMysql_Update
 putlog "\00304\[YnM-Timers\]\003 \002 Betöltve "
